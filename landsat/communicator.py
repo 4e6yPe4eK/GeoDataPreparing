f0f8c0fc4a5115c45b3edbf7c0bd2a73299870dc
4e6yPe4eK <66591568+4e6yPe4eK@users.noreply.github.com>
2024-04-02 23:06:06 +0300

    Co-registration added

--- landsat/communicator.py
+++ landsat/communicator.py
@@ -32,11 +32,6 @@ def parse_directories(data, callback):
 
 
 def process_all(data, callback):
-    # directories = data["directories"]
-    # output = data["output"]
-    # resolution = data["resolution"]
-    # allowed_fields = data["fields"]
-    # allowed_coefficients = data["coefficients"]
     match_fields = data["match_fields"]
     crs, shapes = load_shape(data["shape"])
     data["shape"] = (crs, shapes)
@@ -47,6 +42,8 @@ def process_all(data, callback):
         except Exception as err:
             callback(f"Неизвестная ошибка при обработке директории {directory}! Сообщение: {err}",
                      callback_type="error")
+
+    coregister_all(data, callback)
 
     for field_index, field_shape in enumerate(shapes):
         callback(50 + 50 * field_index // len(shapes), callback_type="percent")
@@ -59,6 +56,67 @@ def process_all(data, callback):
                      callback_type="error")
 
     shutil.rmtree(os.path.join(data["output"], "buffer"))
+
+
+def reproj_match(infile, match, outfile):
+    """Reproject a file to match the shape and projection of existing raster.
+
+    Parameters
+    ----------
+    infile : (string) path to input file to reproject
+    match : (string) path to raster with desired shape and projection
+    outfile : (string) path to output file tif
+    """
+    # open input
+    with rasterio.open(infile) as src:
+        src_transform = src.transform
+
+        # open input to match
+        with rasterio.open(match) as match:
+            dst_crs = match.crs
+
+            # calculate the output transform matrix
+            dst_transform, dst_width, dst_height = calculate_default_transform(
+                src.crs,  # input CRS
+                dst_crs,  # output CRS
+                match.width,  # input width
+                match.height,  # input height
+                *match.bounds,  # unpacks input outer boundaries (left, bottom, right, top)
+            )
+
+        # set properties for output
+        dst_kwargs = src.meta.copy()
+        dst_kwargs.update({"crs": dst_crs,
+                           "transform": dst_transform,
+                           "width": dst_width,
+                           "height": dst_height,
+                           "nodata": 0})
+        # open output
+        with rasterio.open(outfile, "w", **dst_kwargs) as dst:
+            # iterate through bands and write using reproject function
+            for i in range(1, src.count + 1):
+                reproject(
+                    source=rasterio.band(src, i),
+                    destination=rasterio.band(dst, i),
+                    src_transform=src.transform,
+                    src_crs=src.crs,
+                    dst_transform=dst_transform,
+                    dst_crs=dst_crs,
+                    resampling=Resampling.nearest)
+
+
+def coregister_all(data, callback):
+    for coefficient in data["coefficients"]:
+        full_data = {}
+        dates = []
+        pathes = glob.glob(os.path.join(data["output"], "buffer", "*"))
+        standard = os.path.join(pathes[0], coefficient + ".tiff")
+        for directory in pathes[1:]:
+            coefficient_path = os.path.join(directory, coefficient + ".tiff")
+            coefficient_path_old = coefficient_path + ".old"
+            os.rename(coefficient_path, coefficient_path_old)
+            reproj_match(coefficient_path_old, standard, coefficient_path)
+            os.remove(coefficient_path_old)
 
 
 def process_directory(data, path, callback):
